04055fa5527a4551f6ff33ac2f0d3ac7535da89a
wishMeLuck9 <149300259+wishMeLuck9@users.noreply.github.com>
2026-06-18 05:34:12 +0100

    Import official Challenge 5 starter assets

---
+++ Assets/Challenge 5/Scripts/DestroyObjectX.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace OfficialStarterAssets
+{
+public class DestroyObjectX : MonoBehaviour
+{
+    void Start()
+    {
+        Destroy(gameObject, 2); // destroy particle after 2 seconds
+    }
+
+
+}
+}
